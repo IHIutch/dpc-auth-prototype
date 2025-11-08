059d85c65b4278362a82fd94d14fea5fa02158bf
JB Hutch <jbhutch01@gmail.com>
2025-11-08 09:16:04 -0500

    Remove unused files

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
-import Header from '../components/Header'
 import { DPCProvider } from '../contexts/DPCContext'
 
 import appCss from '../styles.css?url'
@@ -40,7 +39,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <DPCProvider>
-          <Header />
           {children}
           <TanStackDevtools
             config={{
